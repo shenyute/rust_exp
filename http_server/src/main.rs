c73bab6fb400a1fc10ed193b5b7801cb9ce35c14
YuTeh Shen <shenyute@gmail.com>
2016-06-10 23:08:02 +0800

    add basic simple https server.

---
+++ http_server/src/main.rs
@@ -0,0 +1,50 @@
+extern crate hyper;
+extern crate env_logger;
+
+use std::io::copy;
+
+use hyper::{Get, Post};
+use hyper::net::Openssl;
+use hyper::server::{Server, Request, Response};
+use hyper::uri::RequestUri::AbsolutePath;
+
+macro_rules! try_return(
+    ($e:expr) => {{
+        match $e {
+            Ok(v) => v,
+            Err(e) => { println!("Error: {}", e); return; }
+        }
+    }}
+);
+
+fn echo(mut req: Request, mut res: Response) {
+    match req.uri {
+        AbsolutePath(ref path) => match (&req.method, &path[..]) {
+            (&Get, "/") | (&Get, "/echo") => {
+                try_return!(res.send(b"Try POST /echo"));
+                return;
+            },
+            (&Post, "/echo") => (), // fall through, fighting mutable borrows
+            _ => {
+                *res.status_mut() = hyper::NotFound;
+                return;
+            }
+        },
+        _ => {
+            return;
+        }
+    };
+
+    let mut res = try_return!(res.start());
+    try_return!(copy(&mut req, &mut res));
+}
+
+fn main() {
+    env_logger::init().unwrap();
+    let ssl_context =
+      Openssl::with_cert_and_key("/home/ytshen/proj/rust/rust_exp/cert.pem",
+          "/home/ytshen/proj/rust/rust_exp/key.pem").unwrap();
+    let server = Server::https("0.0.0.0:8443", ssl_context).unwrap();
+    let _guard = server.handle(echo);
+    println!("Listening on http://127.0.0.1:8443");
+}
